68c59ad0e22a290cdff73c04ad079b65fcde0019
xubeihe <1350561040@qq.com>
2017-05-30 21:39:13 +0800

    Initial commit

---
+++ app/src/main/java/com/ldpoly/litepaltest/Category.java
@@ -0,0 +1,25 @@
+package com.ldpoly.litepaltest;
+
+/**
+ * Created by Administrator on 2017/5/24 0024.
+ */
+
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+
+}
---
+++ app/src/main/java/com/ldpoly/litepaltest/MainActivity.java
@@ -0,0 +1,90 @@
+package com.ldpoly.litepaltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        Button addData = (Button) findViewById(R.id.add_data);
+        Button updateData = (Button) findViewById(R.id.updata_data);
+        Button deleteButton = (Button) findViewById(R.id.dalata_data);
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Book> books= DataSupport.findAll(Book.class);
+                for (Book book: books) {
+                    Log.d("MainActivity", "book name is" + book.getName());
+                    Log.d("MainActivity", "book author is" + book.getAuthor());
+                    Log.d("MainActivity", "book pages is" + book.getPages());
+                    Log.d("MainActivity", "book price is" + book.getPrice());
+                    Log.d("MainActivity", "book press is" + book.getPress());
+
+
+                }
+
+            }
+        });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataSupport.deleteAll(Book.class,"price < ?","15");
+            }
+        });
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Book book = new Book();
+
+                book.setName("The Loset Symbol");
+                book.setAuthor("Dan Brown");
+                book.setPages(510);
+                book.setPrice(19.95);
+                book.setPress("Unknow");
+                book.save();
+                book.setPrice(10.99);
+                book.save();
+
+                book.setPrice(14.95);
+                book.getPress();
+                book.updateAll("neme = ? and author = ?,The Lost Symbol","Dan Brown");
+
+
+
+            }
+        });
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setName("The Da Vinci Code");
+                book.setAuthor("Dan Brown");
+                book.setPages(454);
+                book.setPrice(16.96);
+                book.setPress("Unknow");
+                book.save();
+            }
+        });
+        createDatabase.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+                LitePal.getDatabase();
+            }
+        });
+    }
+}
